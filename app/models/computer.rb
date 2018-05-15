3336bf420587655393e47eee310d354716d8df11
Tech-A64 <tech-a64@Tech-A64s-MacBook-Air.local>
2018-05-15 20:12:04 +0100

    Validations

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,3 +1,3 @@
 class Computer < ApplicationRecord
-  validates :serial_number, :with => /^[A-Za-z0-9.&]*\z/
+  validates :serial_number, format:  { with: /\A[a-zA-Z0-9]\z/, message: "No special characters Please!" }
 end

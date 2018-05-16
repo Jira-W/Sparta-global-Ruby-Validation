c06f337ef314083434da6b61f7720d2270af8c36
Tech-A64 <tech-a64@Tech-A64s-MacBook-Air.local>
2018-05-16 10:23:09 +0100

    Refine regular expression

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,3 +1,3 @@
 class Computer < ApplicationRecord
-  validates :serial_number, format:  { with: /\A[a-zA-Z0-9]\z/, message: "No special characters Please!" }
+  validates :serial_number, format:  { with: /\A\w\z*/ , message: "No special characters Please!" }
 end
